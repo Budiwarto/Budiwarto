1fe9f5df32d5ec115edbe80e8a4b93be58b83ea5
Budiwarto <114948427+Budiwarto@users.noreply.github.com>
2022-10-04 01:22:41 +0700

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Budiwarto
+- 👀 I’m interested in dieng central of java
+- 🌱 I’m currently learning potatoseed
+- 💞️ I’m looking to collaborate on your company
+- 📫 How to reach me with email
+
+<!---
+Budiwarto/Budiwarto is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
